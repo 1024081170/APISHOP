8cf758bc3d3a1891d6b92c391a7e2ec0dbd9a8a4
1024081170 <1024081170@gmail.com>
2019-06-18 00:50:20 +0000

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    客户端
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,16 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
+//对称加密
+Route::get('/guzzle', 'TestController@guzzle');
+//非对称加密
+Route::get('/test_rsa', 'TestController@test_rsa');
+//签名
+Route::get('/signature', 'TestController@signature');
+
+
+
+Route::resource('/posts','PostController@index');
+
